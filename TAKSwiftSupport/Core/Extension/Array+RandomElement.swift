998e9672005a82ec60f3d2871271e4d3c418d49e
yamamotosaika <yamamotosaika@team-lab.com>
2015-12-24 17:00:03 +0900

    Array用の拡張クラスを追加

---
+++ TAKSwiftSupport/Core/Extension/Array+RandomElement.swift
@@ -0,0 +1,23 @@
+//
+//  Array+RandomElement.swift
+//  TAKSwiftSupport
+//
+//  Created by yamamotosaika on 2015/12/24.
+//  Copyright © 2015年 TakuNishimura. All rights reserved.
+//
+
+import UIKit
+
+public extension Array {
+
+    /**
+     指定された個数分、重複なしで元の配列から要素を取り出し、配列を再生成
+     
+     - parameter number: 取り出す個数
+     
+     - returns: 配列
+     */
+    func randomItem(itemNum: Int) -> [Any] {
+        
+    }
+}
